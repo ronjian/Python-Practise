55c160a83cbdec2de8504a5bc35f2f0b31f73748
Jiang, Rong <835293711@qq.com>
2017-09-21 09:09:38 +0800

    9/19 add comments

--- codility/test.py
+++ codility/test.py
@@ -1,21 +1,15 @@
 A = [0,1,2,2,3,5]
 B = [500000,500000,0,0,0,20000]
 
-A = [0, 0]
-B = [0, 0]
 
 def solution(A, B):
     C = [0]*len(A)
     count = 0
-    D = []
     for i in range(len(A)):
         C[i] = A[i] + B[i]/float(1000000)
         for j in range(0,i):
             if C[j] * C[i]>= C[j] + C[i]:
                 count += 1
-                D.append((j,i))
-    print(C)
-    print D
     return count
 
 
@@ -23,3 +17,5 @@ def solution(A, B):
 
 
 print(solution(A, B))
+
+#https://stackoverflow.com/questions/25750240/how-to-find-pairs-with-product-greater-than-sum
